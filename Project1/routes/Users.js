00d0c5852b13f225907a3b4be6fdc1dea5407195
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-04-13 22:01:55 +0530

    updated user routes

--- Project1/routes/Users.js
+++ Project1/routes/Users.js
@@ -15,90 +15,80 @@ users.use(cors());
 process.env.SECRET_KEY = 'secret'; 
 
 
-
-users.post ('/register', (req, res) => {
-
-    const today = new Date ();
-    const userData = {
+//insert
+users.post('/register', async (req, res) => {
+    try {
+      const today = new Date();
+      const userData = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
         password: req.body.password,
         created: today
+      };
+  
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        const hash = await bcrypt.hash(req.body.password, 10);
+        userData.password = hash;
+        const newUser = await User.create(userData);
+        res.json({ status: newUser.email + " registered" });
+      } else {
+        res.json({ error: "User Already Registered" });
+      }
+    } catch (err) {
+      res.send("error" + err);
     }
+  });
+  
 
-    User.findOne( {
-        email: req.body.email
-    })
-        .then(user => {
-            if (!user){
-                bcrypt.hash(req.body.password, 10, (err,hash) => {
-                    userData.password = hash
-                    User.create(userData)
-                    .then(user => {
-                        res.json({status: user.email + " registered"})
-                    })
-                    .catch(err => {
-                        res.send("error" + err);
-                    })
-                })
-            } else {
-                res.json({error: "User Already Registered"})
-            }
-        })
-        .catch(err => {
-            res.send("error" + err);
-        })
-            
-})
-
-users.post('/login', (req, res) => {
-    User.findOne ({
-        email: req.body.email
-    })
-    .then (user => {
-        if(user){
-            if(bcrypt.compareSync(req.body.password, user.password)){
-                const payload = {
-                    _id : user._id,
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    email: user.email
-                }
-                let token = jwt.sign(payload, process.env.SECRET_KEY, {
-                    expiresIn: 1140
-                })
-                res.send(token)
-            }else{
-                res.json({error: "User does not exit in the system"})
-            }
-        }else{
-            res.json({error: "User does not exit in the system"})
+  users.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          const payload = {
+            _id: user._id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+          };
+          const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: 1140 });
+          res.send(token);
+        } else {
+          res.json({ error: "Incorrect password" });
         }
-    })
-    .catch(err => {
-        res.send("error"+err);
-    })
-})
+      } else {
+        res.json({ error: "User does not exist in the system" });
+      }
+    } catch (err) {
+      res.send("error" + err);
+    }
+  });
+  
 
 
 
-users.get('/profile', (req,res) => {
-    var decoded = jwt.verify(req.headers('authorization'), process.env.SECRET_KEY)
-    User.findOne({
-        _id: decoded._id
-    })
-    .then ( user => {
-        if (user){
-            res.json(user)
-        }else{
-            res.send("User does not exit");
-        }
-    })
-    .catch(err => {
-        res.send("Error" + err)
-    })
+  users.get('/profile', async (req, res) => {
+    try {
+      const token = req.headers['authorization'];
+      if (!token) {
+        res.send("Invalid User");
+        return;
+      }
+  
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      const user = await User.findOne({ _id: decoded._id });
+  
+      if (user) {
+        res.json(user);
+      } else {
+        res.send("Invalid User");
+      }
+    } catch (err) {
+      res.send("Error" + err);
+    }
+  });
+  
 
-})
-
-module.exports =users;
+module.exports = users;
