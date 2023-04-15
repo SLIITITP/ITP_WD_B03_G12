0d3c17e958eab944cd59114b0528ad69f3fb6281
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-04-15 23:50:58 +0530

    updated withRouter.js component

--- Project1/myapp/src/components/withRouter.js
+++ Project1/myapp/src/components/withRouter.js
@@ -1,12 +1,16 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const withRouter = (Component) => {
   const Wrapper = (props) => {
     const navigate = useNavigate();
-    
+    const location = useLocation();
+
+
     return (
       <Component
         navigate={navigate}
+        location={location}
+        
         {...props}
         />
     );
