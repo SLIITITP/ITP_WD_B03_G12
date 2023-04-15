0597839800d3870a6ae70dc64025db80576d99a9
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-04-15 23:49:57 +0530

    updated UserFunction.js component

--- Project1/myapp/src/components/UserFunction.js
+++ Project1/myapp/src/components/UserFunction.js
@@ -2,32 +2,48 @@ import axios from "axios";
 
 export const register = newUser => {
     return axios
-    .post ('/users/register', {
-        first_name : newUser.first_name,
+      .post("users/register", {
+        first_name: newUser.first_name,
         last_name: newUser.last_name,
         email: newUser.email,
-        password: newUser.password
-    })
-    .then (res => {
-        console.log("Registered Successfully");
-        window.alert("Registered Successfully");
-    })
-}
+        password: newUser.password,
+        privilege: "USER"
+      })
+      .then(res => {
+        if (res.data && res.data.registered) {
+          window.alert("This email is already registered. Please log in or use a different email.");
+        } else {
+          console.log("Registered Successfully. here1");
+          window.alert("Registered Successfully. here1");
+          return { registered: true };
+        }
+      })
+      .catch(err => {
+        if (err.response && err.response.data && err.response.data.error) {
+          window.alert(err.response.data.error);
+        } else {
+          console.log("Error:", err);
+        }
+      });
+  };
 
 export const login = user => {
     return axios 
-    .post('/users/login', {
+    .post('users/login', {
         email: user.email,
         password: user.password
     })
     .then(res => {
-        localStorage.setItem('usertoken', res.data)
-        return res.data
+        if (res.data.success) {
+            localStorage.setItem('usertoken', res.data.token);
+        }
+        return res.data;       
     })
-    .catch(err => {
+    .catch(err => {     
         console.log(err)
     })
 
 
 }
+
 
