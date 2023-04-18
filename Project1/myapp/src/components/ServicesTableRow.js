7d71ac1243ed023c3e2405ca2a8d0fd524fb3689
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-04-18 16:20:44 +0530

    updated ServicesTableRow

--- Project1/myapp/src/components/ServicesTableRow.js
+++ Project1/myapp/src/components/ServicesTableRow.js
@@ -6,57 +6,19 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import { withRouter } from "./withRouter";
 
-function UpdateModel(props) {
-  const onUpdate = (id) => {
-    axios.put(`http://localhost:5000/service/update/${id}`).then((res) => {
-      alert(`updating : ${id}`);
-    });
-  };
-
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="name@example.com"
-              autoFocus
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={onUpdate}>Update</Button>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
 const ServicesTableRow = (props) => {
   const [serviceState] = useState({
+    _id: props.obj._id,
     service_name: props.obj.service_name,
     service_price: props.obj.service_price,
   });
 
-  const [modalShow, setModalShow] = React.useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [updated, setUpdated] = useState({});
 
   const onDelete = (id) => {
     axios.get(`http://localhost:5000/service/delete/${id}`).then((res) => {
@@ -65,21 +27,90 @@ const ServicesTableRow = (props) => {
     });
   };
 
-  const onUpdateReq = (serviceState) => {
+  const updateService = (serviceState) => {
     console.log(serviceState);
-    setModalShow(true);
+    setUpdated(serviceState);
+
+    handleShow();
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setUpdated((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const onUpdate = (_id) => {
+    axios
+      .put(`http://localhost:8000/service/update/${_id}`, updated)
+      .then((res) => {
+        alert(`Updated Successfully : ${_id}`);
+        handleClose();
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <tr>
-      <UpdateModel show={modalShow} onHide={() => setModalShow(false)} />
+      {
+        //-------------------------Update form using bootstrap Modal-------------------
+      }
+
+      <Modal {...props} size="lg" show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Update Services
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Service Name:</Form.Label>
+              <Form.Control
+                type="text"
+                name="service_name"
+                value={updated.service_name}
+                onChange={handleChange}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Service Price</Form.Label>
+              <Form.Control
+                type="text"
+                name="service_price"
+                value={updated.service_price}
+                onChange={handleChange}
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => onUpdate(serviceState._id)}>Update</Button>
+          <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      {
+        //-------------------------Display All data -------------------
+      }
+
+      <td key={serviceState._id} style={{ display: "none" }}>
+        {" "}
+      </td>
       <td>{serviceState.service_name}</td>
       <td>{serviceState.service_price}</td>
       <td>
         <button
           type="submit"
           className="submit"
-          onClick={() => onUpdateReq(serviceState)}
+          onClick={() => updateService(serviceState)}
         >
           <Link className="nav-link">Update</Link>
         </button>
@@ -88,7 +119,7 @@ const ServicesTableRow = (props) => {
         <button
           type="submit"
           className="delete"
-          onClick={() => onDelete(props.obj._id)}
+          onClick={() => onDelete(serviceState._id)}
         >
           <Link className="nav-link">Delete</Link>
         </button>
