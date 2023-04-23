2fc1fbadf76634c7a47e4a61f671fcface48241f
Anupa Herath <it21295560@my.sliit.lk>
2023-04-24 01:26:00 +0530

    Item.js updated

--- Project1/models/Items.js
+++ Project1/models/Items.js
@@ -1,93 +1,52 @@
-const express = require('express');
-const itemRoutes = express.Router(); 
+const { Double } = require("mongodb");
 
-let Item = require ( "../models/Items");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-//insert
-itemRoutes.route('/add').post(function(req,res) {
-    let item = new Item(req.body);
-    item.save()
-        .then(item => {
-            res.status(200).json({'item': 'item added succesfully'});
-        })
-        .catch (err => {
-            res.status(400).send ("Unable to save")
-        })
-})
+const ItemSchema = new Schema ({
 
-//read
-itemRoutes.route('/').get(async function (req, res) {
-    try{
-        const item = await Item.find();
-        res.json(item);
-    }
-    catch{
-        console.log(error);
-    }
-})
+    
 
-//delete
-itemRoutes.route('/delete/:id').get(async (req, res) => {
-    try {
-      const item = await Item.findByIdAndRemove({ _id: req.params.id });
-      if (item) {
-        res.json('Successfully removed');
-      } else {
-        res.json('Item not found');
-      }
-    } catch (err) {
-      res.json(err);
-    }
-  });
+    name: {
+        type: String
+    } ,
 
-  
-  // Update 
-itemRoutes.route('/update/:id').put(async (req, res) => {
-    try {
-      const item = await Item.findById(req.params.id);
-  
-      if (!item) {
-        return res.status(404).json({ error: 'Item not found' });
-      }
-  
-      item. name = req.body.item_name;
-      item. category = req.body.item_category;
-      item. price  = req.body.item_price;
-      item. Supplier = req.body.item_Supplier;
-      item.description = req.body.item_description;
-      item. qty = req.body.item_qty;
-      item.manufacture_date = req.body.item_manufacture_date;
-      item. expire_date = req.body.item_expire_date;
-     
+    category: {
+        type: String
+    } ,
+    
+    price: {
+        type: String,
+        required: true
+    },
 
-      await item.save();
-      res.json(item);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
+    Supplier: {
+        type: String,
+        required: true
+    } ,
+    
+    description: {
+        type: String
+       
+    },
 
+    qty: {
+        type: String,
+        required: true
+    },
 
+   manufacture_date: {
+        type: Date,
+      
+    },
 
-
- //count
- itemRoutes.route('/get/count').get(async function (req, res) {
-  try {
-    const count = await Item.countDocuments();
-    res.json(count);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Server error");
-  }
+    expire_date: {
+        type: Date,
+        
+    },
+}, {
+    collection: 'item'
 });
 
 
-
-
-module.exports = itemRoutes;
-
-
-
-
-
-
+module.exports  =mongoose.model ('Item', ItemSchema);
