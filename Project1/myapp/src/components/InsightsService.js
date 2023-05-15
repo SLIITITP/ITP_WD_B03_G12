f37cefeaecc82d9047ac7cb481f9f87915ac667f
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-05-15 11:52:05 +0530

    Insights modified

--- Project1/myapp/src/components/InsightsService.js
+++ Project1/myapp/src/components/InsightsService.js
@@ -9,7 +9,7 @@ const InsightsService = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/service/');
+        const response = await axios.get('http://localhost:5000/income/');
         setService(response.data);
       } catch (error) {
         console.log(error);
@@ -23,8 +23,8 @@ const InsightsService = () => {
 
   // Transform service data into an array of data points
   const data = service.map((item) => ({
-    name: item.service_name,
-    value: parseFloat(item.service_price),
+    name: item.type,
+    value: parseFloat(item.total),
   }));
 
   const [asideActive, setAsideActive] = useState(false);
@@ -33,19 +33,227 @@ const InsightsService = () => {
     setAsideActive(!asideActive);
   };
 
+  const [countEmp, setCountEmp] = useState(0);
+
+useEffect(() => {
+  axios
+    .get("http://localhost:5000/employee/get/count")
+    .then((response) => {
+      console.log(response);
+      setCountEmp(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+const [countAdmission, setCountAdmission] = useState(0);
+
+useEffect(() => {
+  axios
+    .get("http://localhost:5000/admission/get/count")
+    .then((response) => {
+      console.log(response);
+      setCountAdmission(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+const [countAnimal, setCountAnimal] = useState(0);
+
+useEffect(() => {
+  axios
+    .get("http://localhost:5000/animal/get/count")
+    .then((response) => {
+      console.log(response);
+      setCountAnimal(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+const [countAppointment, setCountAppointment] = useState(0);
+
+useEffect(() => {
+  axios
+    .get("http://localhost:5000/appointments/get/count")
+    .then((response) => {
+      console.log(response);
+      setCountAppointment(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+const [countAccounts, setCountAccounts] = useState(0);
+
+useEffect(() => {
+  axios
+    .get("http://localhost:5000/accounts/get/count")
+    .then((response) => {
+      console.log(response);
+      setCountAccounts(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+const [countInpatient, setCountInpatient] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/inpatient/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountInpatient(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const [countItem, setCountItem] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/item/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountItem(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const [countOrder, setCountOrder] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/adminOrder/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountOrder(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const [countService, setCountService] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/service/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountService(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+  const [countShelter, setCountShelter] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/shelters/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountShelter(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const [countSupplier, setCountSupplier] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/supplier/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountSupplier(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const [countUsers, setCountUsers] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/users/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+  const [countVaccine, setCountVaccine] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/vaccine/get/count")
+      .then((response) => {
+        console.log(response);
+        setCountVaccine(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+
   return (
     <>
-    <div>
-      <h1>Service Insights</h1>
-
-      <ResponsiveContainer width="100%" height={400}>
-        <PieChart width={400} height={400}>
+<div style={{ display: "flex" }}>
+  <div style={{width:"35em", background: "pink"}}>
+    <h1>OverView</h1>
+    <h3>Employees: {countEmp}</h3>
+    <h3>Registered Animals: {countAnimal}</h3>
+    <h3>Registered Users: {countUsers}</h3>
+    <h3>Inpatients: {countInpatient}</h3>
+    <h3>All Items: {countItem}</h3>
+    <h3>Suppliers: {countSupplier}</h3>
+    <h3>Ongoing Order: {countOrder}</h3>
+    <h3>Ongoing Appointments: {countAppointment}</h3>
+    <h3>Vaccine: {countEmp}</h3>
+  </div>
+  <div>
+    <h1>Income Insights</h1>
+    <ResponsiveContainer width="100%" height={400}>
+      <PieChart width={400} height={400}>
         <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-           <Pie data={data} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-      
-        </PieChart>
-      </ResponsiveContainer>
-    </div> 
+        <Pie
+          data={data}
+          dataKey="value"
+          cx="50%"
+          cy="50%"
+          innerRadius={70}
+          outerRadius={90}
+          fill="#82ca9d"
+          label
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  </div>
+</div>
+
 
    <Insights/>
     <tbody>{Insights()}</tbody>
