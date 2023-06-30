24b515d115805fd470b1dc6e6a342bd1df6afd66
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-06-30 18:16:10 +0530

    modified  profile

--- Project1/myapp/src/components/Profile.js
+++ Project1/myapp/src/components/Profile.js
@@ -1,71 +1,88 @@
-import React, { Component } from 'react'
-import jwt_decode from 'jwt-decode'
+import React, { Component } from "react";
+import jwt_decode from "jwt-decode";
+import { Row, Col } from "react-bootstrap";
 
 class Profile extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      first_name: '',
-      last_name: '',
-      email: '',
-      error: '',
-    }
+      first_name: "",
+      last_name: "",
+      email: "",
+      error: "",
+    };
   }
 
   componentDidMount() {
     const token = localStorage.usertoken;
-    console.log('token:', token);
+    console.log("token:", token);
     if (!token) {
-      this.setState({ error: 'Token not found in local storage' })
-      return
+      this.setState({ error: "Token not found in local storage" });
+      return;
     }
 
     try {
-      const decoded = jwt_decode(token)
+      const decoded = jwt_decode(token);
       this.setState({
         first_name: decoded.first_name,
         last_name: decoded.last_name,
         email: decoded.email,
-      })
+      });
     } catch (error) {
-      this.setState({ error: 'Error decoding token: ' + error.message })
+      this.setState({ error: "Error decoding token: " + error.message });
     }
   }
 
   render() {
-    const { error, first_name, last_name, email } = this.state
+    const { error, first_name, last_name, email } = this.state;
     if (error) {
-      return <div>Error: {error}</div>
+      return <div>Error: {error}</div>;
     }
 
     return (
-      <div className='container'>
-        <div className='jumbotron mt-5'>
-          <div className='col-sm8 mx-auto'>
-            <h1 className='text-center'>Profile</h1>
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm8 mx-auto">
+            <h1 className="text-center">Profile</h1>
 
-            <table className='table col-md-6 mx-auto'>
-              <tbody>
-                <tr>
-                  <td>First Name</td>
-                  <td>{first_name} </td>
-                </tr>
+            <Row style={{ marginLeft: "200px" }}>
+              <Col md={3}>
+                <img
+                  src="../img/user.png"
+                  className="circular-image"
+                  style={{ marginTop: "10px" }}
+                  alt="profile"
+                />
+              </Col>
+              <Col md={9}>
+                <table className="table col-md-6 mx-auto">
+                  <tbody>
+                    <h3>
+                      <br />
 
-                <tr>
-                  <td>Last Name</td>
-                  <td>{last_name} </td>
-                </tr>
-
-                <tr>
-                  <td>Email</td>
-                  <td>{email} </td>
-                </tr>
-              </tbody>
-            </table>
+                      <tr>
+                        <td>Name</td>
+                        <td>
+                          <b>
+                            {first_name} {last_name}
+                          </b>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Email</td>
+                        <b>
+                          <td>{email}</td>
+                        </b>
+                      </tr>
+                    </h3>
+                  </tbody>
+                </table>
+              </Col>
+            </Row>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
