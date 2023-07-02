4b513c970d2944cacd36ad3769b544d90b5be95f
Linal Silva <it21374838@my.sliit.lk>
2023-07-02 09:48:33 +0530

    modified Landing.js

--- Project1/myapp/src/components/Landing.js
+++ Project1/myapp/src/components/Landing.js
@@ -1,49 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isParagraphVisible: false,
+    };
+  }
+
+  componentDidMount() {
+    // Delaying the visibility of the paragraph using a timeout
+    setTimeout(() => {
+      this.setState({ isParagraphVisible: true });
+    }, 2000);
+  }
+
   render() {
+    const { isParagraphVisible } = this.state;
     return (
-
       <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="../img/HomeSlide1.png"
-          alt="First slide"
+   <div style={{ backgroundColor: '#f2f2f2', padding: '20px' }}>
+          <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Welcome to Happy Paws!</h2>
+          {isParagraphVisible && (
+            <p style={{ textAlign: 'center', fontSize: '18px', color: 'black', fontStyle: 'italic' }}>
+              Working with animals, despite the regular contretemps, adds up to a rewarding and fulfilling life, which is confirmed by the fact that most vets seem to be happy men.
+            </p>
+          )}
+        </div>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="../img/HomeSlide1.png"
+            alt="First slide"
+          />
        
-        />
-        <Carousel.Caption>
-         
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="../img/HomeSlide3.jpg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-         
-         
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="../img/HomeSlide2.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-        
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="../img/HomeSlide3.jpg"
+            alt="Second slide"
+          />
           
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-     
-    )
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="../img/HomeSlide2.jpg"
+            alt="Third slide"
+          />
+        
+        </Carousel.Item>
+
+      </Carousel>
+      
+      
+    );
   }
 }
 
